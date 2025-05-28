c7aea9ad9a298376190c0b63e6deda93d8347df4
virensharma14 <96727156+virensharma14@users.noreply.github.com>
2025-05-28 19:17:41 +0530

    2pointers based recursion to reverse array

---
+++ reverse_recursion.py
@@ -0,0 +1,30 @@
+def swap(x,y):
+    temp=x
+    x=y
+    y=temp
+    return x,y
+
+def reverse_arr(arr,i,r):
+    if i>=r:
+        return 
+    arr[i],arr[r]=swap(arr[i],arr[r])
+    reverse_arr(arr,i+1,r-1)
+
+
+def main():
+    arr=[1,5,6,9,10]
+    i=0
+    r=len(arr)-1
+    reverse_arr(arr,i,r)
+    print(arr)
+
+
+
+
+
+
+
+
+
+if __name__=="__main__":
+    main()
